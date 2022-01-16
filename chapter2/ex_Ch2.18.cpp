5b7e2b056558d23906c8e62b170903ce28d143a0
Anthony Snelling <anthony.snelling@live.com>
2022-01-15 22:00:39 -0500

    added more text explaining references and pointers

--- chapter2/ex_Ch2.18.cpp
+++ chapter2/ex_Ch2.18.cpp
@@ -12,6 +12,10 @@ int main(){
     std::cout << "val1: " << val1 << std::endl;
     std::cout << "pointer: " << *pointer << std::endl; 
 
+    std::cout << "ref: " << ref << std::endl; 
+    std::cout << "&ref: " << &ref << std::endl; 
+    std::cout << std::endl; 
+
     pointer = &ref; // changes what the pointer is pointing too
     *pointer = 20; // changes the value of what the pointer is pointing at
 
@@ -19,5 +23,24 @@ int main(){
     std::cout << "changed pointer value: " << *pointer << std::endl; 
     std::cout << "changed val2: " << val2 << std::endl;
     
+    std::cout << std::endl; 
+    std::cout << "Some other examples of changing values"<< std::endl; 
+    int val3 = 12;
+    int* pointer2 = &val3;
+    int& ref2 = val3;
+    std::cout << "val3: " << val3 << std::endl;
+    std::cout << "ref2: " << ref2 << std::endl;
+    std::cout << "&ref2: " << &ref2 << std::endl;
+    std::cout << "pointer: " << pointer2 << std::endl;
+    std::cout << "*pointer: " << *pointer2 << std::endl;
+
+    std::cout << std::endl; 
+    std::cout << "changing val3 using a pointer"<< std::endl; 
+    *pointer2 = 24;
+    std::cout << "pointer change val3: " << val3 << std::endl; 
+    std::cout << "changing val3 but with reference"<< std::endl; 
+    ref2 = 99; 
+    std::cout << "reference change val3: " << val3 << std::endl; 
+
     return 0;
 }
